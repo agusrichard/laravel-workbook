3f99eb8aa95b9c2576220d3d1c7af0e62f239570
agusrichard <agus.richard21@gmail.com>
2020-04-27 20:17:33 +0700

    Learn 4

--- pizzahouse/routes/web.php
+++ pizzahouse/routes/web.php
@@ -14,11 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::get('/pizzas', function () {
-    return view('pizzas');
-    // return 'pizzas';
-    // return ['Agus' => 'Richard', 'Sekardayu' => 'Hana'];
+  $pizza = ['type' => 'Supreme', 'base' => 'Cheesy crust', 'price' => 10];
+  return view('pizzas', $pizza);
 });
